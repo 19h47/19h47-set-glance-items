c71d060208a8f66c72993905f33f4639a1bab884
Levron Jérémy <jeremylevron@19h47.fr>
2019-10-03 15:45:47 +0200

    :tada: Initial commit

---
+++ set-glance-items.php
@@ -0,0 +1,15 @@
+<?php // phpcs:ignore
+/**
+ * Plugin Name: Set glance items
+ * Plugin URI: https://github.com/19h47/19h47-set-glance-items
+ * Description: Set glance items
+ * Version: 1.0.0
+ * Author: Jérémy Levron (jeremylevron@19h47.fr)
+ * Author URI: https://www.19h47.fr
+ *
+ * @link https://github.com
+ * @since 1.0.0
+ * @package 19h47/set-glance-items
+ */
+
+require plugin_dir_path( __FILE__ ) . 'admin/class-set-glance-items.php';
